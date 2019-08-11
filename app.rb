ef3561836afdad001ce7a8bb29f332c20330e585
Jen <jenniferanndcb@gmail.com>
2019-08-11 15:55:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,12 +19,12 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end 
   
-  get "/multiply/:num1/:num2" do
-    @num1 = params[:num1].to_i 
-    @num2 = params[:num2].to_i
-    
-    @product = @num1 * @num2 
-    "#{product}"
-  end  
+  get '/multiply/:num1/:num2' do
+
+ 		@num1 = params[:num1]
+    @num2 = params[:num2]
+		@machine = @num1.to_i * @num2.to_i
+		"#{@machine}"
+	end 
   
 end
